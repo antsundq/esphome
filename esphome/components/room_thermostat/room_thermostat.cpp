393a27e795b9b7e618f32fd7c4445448cfefa09e
Anton Sundqvist <anton.sundqvist@astemes.com>
2024-03-04 21:57:36 +0200

    first test

---
+++ esphome/components/room_thermostat/room_thermostat.cpp
@@ -0,0 +1,19 @@
+#include "room_thermostat.h"
+#include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
+
+namespace esphome {
+namespace room_thermostat {
+
+void RoomThermostat::setup(){
+    pinMode(5, OUTPUT);
+
+}
+
+void RoomThermostat::update() {
+    self->state_ != self->state_;
+    digital_write(5, self->state_);
+}
+
+}  // namespace room_thermostat
+}  // namespace esphome
---
+++ esphome/components/room_thermostat/room_thermostat.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace room_thermostat {
+
+class RoomThermostat : public PollingComponent {
+public:
+    void setup() override;
+    void update() override;
+
+protected:
+  bool state_;
+  //sensor::Sensor *temperature_sensor_{nullptr};
+  //sensor::Sensor *humidity_sensor_{nullptr};
+}
+
+
+}  // namespace room_thermostat
+}  // namespace esphome
